669995ca3542a0da275a17519a678aa3e64f6d18
iankerkhove <ian.kerkhove@student.ehb.be>
2017-10-23 22:28:35 +0200

    gui
    adding gui

---
+++ we6.spotify/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using we6.spotify.logic;
+
+namespace we6.spotify
+{
+    public partial class Form1 : Form
+    {
+        private SpotifySearch spotifySearch;
+
+        public Form1()
+        {
+            spotifySearch = new SpotifySearch();
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Task<List<string>> t = new Task<List<string>>(() => spotifySearch.Search(textBox1.Text));
+            t.Start();
+            await t;
+
+            foreach (var element in t.Result)
+                listBox1.Items.Add(element);
+        }
+    }
+}
