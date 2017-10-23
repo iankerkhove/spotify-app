662dc44a5aadef07badab26918be8bed731baa34
iankerkhove <ian.kerkhove@student.ehb.be>
2017-10-23 22:30:33 +0200

    logic
    added logic and sln file

---
+++ we6.spotify.logic/SpotifySearch.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace we6.spotify.logic
+{
+    public class SpotifySearch
+    {
+
+        public List<String> Search(string term)
+        {
+            WebClient webClient = new WebClient();
+            JObject data = JObject.Parse(webClient.DownloadString("https://api.spotify.com/v1/search?q=" + term + "&type=artist"));
+
+            return data["artists"]["items"].Select(x => (string)x["name"]).ToList();
+        }
+
+    }
+}
